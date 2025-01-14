1944bae021d72629aecc96ba3d9a21b91730e272
lorevlmt <lore.vimontes@gmail.com>
2025-01-14 14:06:06 +0100

    Ejercicio 24 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -4,15 +4,30 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise24 {
-    public static Queue<String> createQueue() {
 
+    public static Queue<String> createQueue() {
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer("Smith");
+        queue.offer("Montessori");
+        queue.offer("Peralta");
+        queue.offer("House");
+
+        return queue;
     }
+
     public static void printAndEmptyQueue(Queue<String> queue) {
+
+        while (!queue.isEmpty())    //cuando la cola no está vacía
+            System.out.println(queue.poll());   //esto recupera y elimina la cabeza de la cola
 
     }
 
     public static void main(String[] args) {
 
+        Queue<String> queue = createQueue();
+
+        printAndEmptyQueue(queue);
     }
 
 }
